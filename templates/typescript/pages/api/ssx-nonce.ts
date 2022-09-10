e768dfdd45ea640e8ce0cbd1d78c3e66af896560
Juliano <jcezarchagas@gmail.com>
2022-09-10 17:09:27 -0300

    Update ts template

--- templates/typescript/pages/api/ssx-nonce.ts
+++ templates/typescript/pages/api/ssx-nonce.ts
@@ -4,5 +4,5 @@ import ssx from "./_ssx";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const nonce = ssx.generateNonce();
   req.cookies.nonce = nonce;
-  res.status(200).json({ nonce });
+  res.status(200).send(nonce);
 }
